197a99fe1d6eeb726a69192ebd11f7639ce959ac
LORDBABUINO <jorge.x7@gmail.com>
2019-05-11 09:48:01 +0000

    Requests data search from API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,7 +13,8 @@ import { SearchResultComponent } from './search-result/search-result.component';
     SearchResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+		HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResultService } from '../search-result.service'
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.css']
+	styleUrls: ['./search-result.component.css'],
+	providers: [SearchResultService]
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+	items
+
+	constructor(
+		private searchResultService: SearchResultService
+	) { }
 
   ngOnInit() {
+		this.searchResultService.getSearch('petrolina')
+			.subscribe((response: any) => {this.items = response.list})
   }
 
 }
