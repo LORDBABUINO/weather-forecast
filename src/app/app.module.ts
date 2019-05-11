85b97c25414bb9ba600ff3dbf4d41e5606f853ee
LORDBABUINO <jorge.x7@gmail.com>
2019-05-11 07:02:03 +0000

    Inits search-result component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule
